e2cbc9ae4ee9596bc93033403be91cce371b65ec
praveen-banki <prawinbanki7@gmail.com>
2026-08-21 14:12:08 +0530

    Time: 3 ms (19.33%) | Memory: 19.2 MB (66.60%) - LeetSync

--- leetcode/29-divide-two-integers/divide-two-integers.py
+++ leetcode/29-divide-two-integers/divide-two-integers.py
@@ -1,7 +1,9 @@
 class Solution:
     def divide(self, dividend: int, divisor: int) -> int:
-        negative = (dividend < 0) != (divisor < 0)
+        if dividend == -2147483648 and divisor == -1:
+            return 2147483647
 
+        neg = (dividend < 0) != (divisor < 0)
         a = abs(dividend)
         b = abs(divisor)
         ans = 0
@@ -17,13 +19,7 @@ class Solution:
             a -= temp
             ans += count
 
-        if negative:
+        if neg:
             ans = 0 - ans
-
-        if ans > 2147483647:
-            return 2147483647
-
-        if ans < -2147483648:
-            return -2147483648
 
         return ans
