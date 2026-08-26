4e277dcf810286007b84ae088891947169ef4ad8
praveen-banki <prawinbanki7@gmail.com>
2026-08-26 10:58:56 +0530

    Time: 675 ms (9.12%) | Memory: 21.2 MB (43.78%) - LeetSync

--- leetcode/136-single-number/single-number.py
+++ leetcode/136-single-number/single-number.py
@@ -1,6 +1,10 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        ans = 0
+        count = 0
+        new = []
         for i in nums:
-            ans ^= i
-        return ans
+            if i not in new:
+                new.append(i)
+            else:
+                new.remove(i)
+        return new[0]
