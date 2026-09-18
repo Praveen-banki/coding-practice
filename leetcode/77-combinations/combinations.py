6910789eb79169ee48f117fda936fa3734927eaa
praveen-banki <prawinbanki7@gmail.com>
2026-09-18 18:29:23 +0530

    Time: 127 ms (23.32%) | Memory: 61.3 MB (54.91%) - LeetSync

--- leetcode/77-combinations/combinations.py
+++ leetcode/77-combinations/combinations.py
@@ -2,15 +2,12 @@ class Solution:
     def combine(self, n: int, k: int) -> list[list[int]]:
         ans = []
 
-        def solve(start, path):
-            if len(path) == k:
-                ans.append(path[:])
+        def f(start, a):
+            if len(a) == k:
+                ans.append(a[:])
                 return
+            for i in range(start, n + 1):
+                f(i + 1, a + [i])
 
-            for i in range(start, n - (k - len(path)) + 2):
-                path.append(i)
-                solve(i + 1, path)
-                path.pop()
-
-        solve(1, [])
+        f(1, [])
         return ans
